050bd4b067cfaea4b20c125c8739561d0bff4bf2
SShipon <nxshipon@gmail.com>
2023-12-30 11:05:50 +0600

    restaurant basic server create

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u675lb8.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    const restaurantCollection = client.db("My_users").collection("shipon")
+    
+       // users get data client site
+  app.get('/users', async(req, res)=>{
+    const query = restaurantCollection.find()
+    const result = await query.toArray()
+    res.send(result)
+    
+})
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+   // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
